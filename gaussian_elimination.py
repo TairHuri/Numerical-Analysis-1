f6ccd4efeeacf50f782f8ecf080a68e292a9304e
noasht <129064584+noasht@users.noreply.github.com>
2024-02-19 09:27:49 +0200

    Add files via upload

--- gaussian_elimination.py
+++ gaussian_elimination.py
@@ -1,7 +1,16 @@
-# @source:  https://github.com/lihiSabag/Numerical-Analysis-2023.git
+# @source: https://github.com/lihiSabag/Numerical-Analysis-2023.git
+# Date: 02 – 19 – 24
+# Group:
+# Yael Haim         	    324978261
+# Tair Huri 	            325329118
+# Noa Shem Tov              207000134
+# Git: https://github.com/TairHuri/Numerical-Analysis-1
+# Name:  Noa Shem Tov, 207000134
+
 
 import numpy as np
 from colors import bcolors
+from Matrix.inverse_matrix import inverse
 from matrix_utility import swap_row
 
 def gaussianElimination(mat):
@@ -71,8 +80,22 @@ def backward_substitution(mat):
         x[i] = (x[i] / mat[i][i])
 
     return x
+def norm(mat):
+    size = len(mat)
+    max_row = 0
+    for row in range(size):
+        sum_row = 0
+        for col in range(size):
+            sum_row += abs(mat[row][col])
+        if sum_row > max_row:
+            max_row = sum_row
+    return max_row
 
 if __name__ == '__main__':
+
+    print(" @source: https://github.com/lihiSabag/Numerical-Analysis-2023.git\n Date: 02 – 19 – 24 \n Group: \n Yael Haim           324978261"
+          "\n Tair Huri           325329118\n Noa Shem Tov        207000134\n Git: https://github.com/TairHuri/Numerical-Analysis-1"
+          "\n Name:  Noa Shem Tov  207000134")
 
     A_b = [
         [1,  -1,   2,  -1,  -8],
@@ -110,3 +133,12 @@ if __name__ == '__main__':
         print(bcolors.OKBLUE, "\nSolution for the system:")
         for x in result:
             print("{:.6f}".format(x))
+    """norm_A = norm(A)
+       norm_A_inv = norm(A_inverse)
+       condA = norm_A * norm_A_inv
+       if condA > 99:
+           print("Cannot solve, change the conditions")
+       print(bcolors.OKBLUE, "A:", bcolors.ENDC)
+       print_matrix(A)
+       print(bcolors.OKBLUE, "Max Norm of A:", bcolors.ENDC, norm_A, "\n")
+       print(bcolors.OKBLUE, "max norm of the inverse of A:", bcolors.ENDC, norm_A_inv)"""
--- inverse_matrix.py
+++ inverse_matrix.py
@@ -1,4 +1,11 @@
 # @source: https://github.com/lihiSabag/Numerical-Analysis-2023.git
+# Date: 02 – 19 – 24
+# Group:
+# Yael Haim         	    324978261
+# Tair Huri 	            325329118
+# Noa Shem Tov              207000134
+# Git: https://github.com/TairHuri/Numerical-Analysis-1
+# Name:  Noa Shem Tov, 207000134
 
 from colors import bcolors
 from matrix_utility import row_addition_elementary_matrix, scalar_multiplication_elementary_matrix, print_matrix
@@ -63,18 +70,12 @@ def inverse(matrix):
                 identity = np.dot(elementary_matrix, identity)
     return identity
 
-def norm(mat):
-    size = len(mat)
-    max_row = 0
-    for row in range(size):
-        sum_row = 0
-        for col in range(size):
-            sum_row += abs(mat[row][col])
-        if sum_row > max_row:
-            max_row = sum_row
-    return max_row
-
 if __name__ == '__main__':
+
+    print(
+        " @source: https://github.com/lihiSabag/Numerical-Analysis-2023.git\n Date: 02 – 19 – 24 \n Group: \n Yael Haim           324978261"
+        "\n Tair Huri           325329118\n Noa Shem Tov        207000134\n Git: https://github.com/TairHuri/Numerical-Analysis-1"
+        "\n Name:  Noa Shem Tov  207000134")
 
     A = np.array([[2, 3, 1],
                   [3, 4, 2],
@@ -88,12 +89,3 @@ if __name__ == '__main__':
     except ValueError as e:
         print(str(e))
 
-    """norm_A = norm(A)
-    norm_A_inv = norm(A_inverse)
-    condA = norm_A * norm_A_inv
-    if condA > 99:
-        print("Cannot solve, change the conditions")
-    print(bcolors.OKBLUE, "A:", bcolors.ENDC)
-    print_matrix(A)
-    print(bcolors.OKBLUE, "Max Norm of A:", bcolors.ENDC, norm_A, "\n")
-    print(bcolors.OKBLUE, "max norm of the inverse of A:", bcolors.ENDC, norm_A_inv)"""
